9b82e3c839a6f72783667b096e1cf8fcfff348ef
SnusMumriken00 <108468864+SnusMumriken00@users.noreply.github.com>
2022-08-04 14:18:19 +0200

    DoorScript

---
+++ Dog Luigi/Assets/Chief/Scripts/DoorFunctions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface DoorFunctions
+{
+    void OpenDoor();
+    void CloseDoor();
+    void ToggleDoor();
+
+}
