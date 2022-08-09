0c6ce70434d64262a2a9acc59d98bbf195079099
iRessu <109947890+iRessu@users.noreply.github.com>
2022-08-09 20:29:44 +0200

    PBear AI

--- Dog Luigi/Assets/Lukas/EnemyAi.cs
+++ Dog Luigi/Assets/Lukas/EnemyAi.cs
@@ -1,26 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAi : MonoBehaviour
 {
+    Animator anim;
 
+    public Transform player;
+    public float agroRange;
+    public float moveSpeed;
 
-    public float speed;
-    public float stoppingDistance;
-
-    private Transform target;
+    Rigidbody2D rb2d;
 
     private void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        if(Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distToPlayer < agroRange)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            chasePlayer();
+        }
+
+        else
+        {
+            StopChasingPlayer();
         }
     }
+
+     void chasePlayer()
+    {
+        if (transform.position.x < player.position.x) 
+        {
+            rb2d.velocity = new Vector2(moveSpeed, 0);
+           
+        }
+        else 
+        {
+            rb2d.velocity = new Vector2(-moveSpeed, 0);
+            
+        }
+        anim.Play("PBear_RunAN");
+    }
+
+    void StopChasingPlayer()
+    {
+        rb2d.velocity = new Vector2(0, 0);
+    }
 }
+
+
