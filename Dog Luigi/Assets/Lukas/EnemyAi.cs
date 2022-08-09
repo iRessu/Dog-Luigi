28a60e304ae674dfd43a993fb7abedfa7af36c1b
iRessu <109947890+iRessu@users.noreply.github.com>
2022-08-09 20:50:21 +0200

    PBearAI

--- Dog Luigi/Assets/Lukas/EnemyAi.cs
+++ Dog Luigi/Assets/Lukas/EnemyAi.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class EnemyAi : MonoBehaviour
 {
     Animator anim;
+  
 
     public Transform player;
     public float agroRange;
     public float moveSpeed;
+
+
+
 
     Rigidbody2D rb2d;
 
@@ -35,24 +39,26 @@ public class EnemyAi : MonoBehaviour
     }
 
      void chasePlayer()
+
     {
-        if (transform.position.x < player.position.x) 
-        {
-            rb2d.velocity = new Vector2(moveSpeed, 0);
-           
-        }
-        else 
-        {
-            rb2d.velocity = new Vector2(-moveSpeed, 0);
-            
-        }
+            if (transform.position.x < player.position.x)
+            {
+                rb2d.velocity = new Vector2(moveSpeed, 0);
+            }
+            else
+            {
+                rb2d.velocity = new Vector2(-moveSpeed, 0);
+            }
         anim.Play("PBear_RunAN");
     }
 
     void StopChasingPlayer()
     {
         rb2d.velocity = new Vector2(0, 0);
+        anim.Play("PBear_IdleAN");
     }
+
+  
 }
 
 
